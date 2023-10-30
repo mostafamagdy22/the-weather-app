872eba30d71bb8ab69335132d82d9c1f544b7e86
mostafamagdy22 <mostafamagdy12304@gmail.com>
2023-10-30 08:10:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import SiteNav from './components/SiteNav.vue';
+</script>
+
+<template>
+    <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+        <site-nav></site-nav>
+    </div>
+</template>
+
